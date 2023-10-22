75041b59666792437f61cfa15a77c083fe4bc264
yung <panda891028@gmail.com>
2023-10-22 21:07:27 +0800

    update js

--- hw02/src/medium/80ddd5cd24b8c474@57.js
+++ hw02/src/medium/80ddd5cd24b8c474@57.js
@@ -7,7 +7,7 @@ md`### 以 bar chart 呈現每個年份出生的人數 (2pt)	`
 )}
 
 function _data(FileAttachment){return(
-FileAttachment("../../data.json").json()
+FileAttachment("data.json").json()
 )}
 
 function _4(md){return(
@@ -88,7 +88,7 @@ export default function define(runtime, observer) {
   const main = runtime.module();
   function toString() { return this.url; }
   const fileAttachments = new Map([
-    ["data.json", {url: new URL("./files/01d24d9aeda19b7590d996fac1553c965097547ebd857a93a21c0f86efc088993c6168f092b2353e6ec91202e5a6dead5ad66b688c1038a20deb6ec8d35e570d.json", import.meta.url), mimeType: "application/json", toString}]
+    ["data.json", {url: new URL("../../data.json", import.meta.url), mimeType: "application/json", toString}]
   ]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], _1);
