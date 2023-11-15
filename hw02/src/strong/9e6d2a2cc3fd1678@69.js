8130782a57c5adc0ec90d6cf3dfa785fe2de73c4
yung <panda891028@gmail.com>
2023-11-16 01:05:47 +0800

    update js

--- hw02/src/strong/9e6d2a2cc3fd1678@69.js
+++ hw02/src/strong/9e6d2a2cc3fd1678@69.js
@@ -109,7 +109,7 @@ export default function define(runtime, observer) {
   const main = runtime.module();
   function toString() { return this.url; }
   const fileAttachments = new Map([
-    ["data.json", {url: new URL("./files/01d24d9aeda19b7590d996fac1553c965097547ebd857a93a21c0f86efc088993c6168f092b2353e6ec91202e5a6dead5ad66b688c1038a20deb6ec8d35e570d.json", import.meta.url), mimeType: "application/json", toString}]
+    ["data.json", {url: new URL("../data.json", import.meta.url), mimeType: "application/json", toString}]
   ]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], _1);
