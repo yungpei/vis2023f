4539309e0f0e722c64c9cb8c8b5330ec0d37aa90
yung <panda891028@gmail.com>
2023-11-19 21:41:19 +0800

    Update all

--- hw03/src/js/calorie.js
+++ hw03/src/js/calorie.js
@@ -1,63 +1,43 @@
-let totalDistance = 0;
-let totalTime = 0;
+const jsonUrl02 = '../json/AerobicData.json';
 
-function calculateDistance(x1, y1, x2, y2) {
-  // 這裡可以使用適當的算法計算兩點之間的距離，例如歐幾里得距離公式
-  const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-  return distance;
+function loadjson() {
+  fetch(jsonUrl02)
+    .then(response => response.json())
+    .then(data => {
+      calorieData = data;
+      if (calorieIndex < calorieData.length) {
+        const point = calorieData[calorieIndex];
+        updateIconPosition(point.X, point.Y);
+        updateInfo(point.Time, point.Floor, point.Distance, point.Calorie);
+        calorieIndex++;
+      } else {
+        calorieIndex = 0;
+      }
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
 }
 
-function updateCaloriesAndTime(x, y) {
-  if (currentIndex > 0) {
-    const prevPoint = jsonData[currentIndex - 1];
-    const distance = calculateDistance(prevPoint.X, prevPoint.Y, x, y);
-    const speed = 2; // 假設平均步行速度為每秒1公尺
-    const time = distance / speed;
-    
-    totalDistance += distance;
-    totalTime += time;
+// 新增一個函數用來更新資訊
+function updateInfo(time, floor, distance, calorie) {
+  const timeInfo = document.getElementById('time-info');
+  const distanceInfo = document.getElementById('distance-info');
+  const calorieInfo = document.getElementById('calories-info');
 
-    console.log("Total Distance:", totalDistance, "meters");
-    console.log("Total Time:", totalTime, "seconds");
-    console.log("Total Calories:", calculateCalories(totalDistance), "calories");
-  }
+  // 在這裡可以加上計算速度、卡路里等其他相關邏輯
+  const speed = distance / time;
+
+  timeInfo.textContent = `Time: ${time} seconds`;
+  distanceInfo.textContent = `Distance: ${distance} meters,`;
+  calorieInfo.textContent = `Calories: ${calorie} calories`;
+
+  // 這裡可以加上其他相關邏輯
 }
 
-function calculateCalories(distance) {
-  // 這裡可以使用適當的算法計算卡路里，例如基於距離和體重的公式
-  const weight = 55; // 假設體重為55公斤
-  const calories = distance * 0.0005 * weight; // 假設每公尺消耗0.0005卡路里
-  return calories;
-}
-function updateInfo() {
-    document.getElementById('distance-info').innerText = `Total Distance: ${totalDistance.toFixed(2)} meters`;
-    document.getElementById('time-info').innerText = `Total Time: ${totalTime.toFixed(2)} seconds`;
-    document.getElementById('calories-info').innerText = `Total Calories: ${calculateCalories(totalDistance).toFixed(2)} calories`;
-  }
-  
-  // 在 loadjson 函式中的位置更新後呼叫
-  function loadjson() {
-    fetch(jsonUrl)
-      .then(response => response.json())
-      .then(data => {
-        jsonData = data;
-        if (currentIndex < jsonData.length) {
-          const point = jsonData[currentIndex];
-          updateIconPosition(point.X, point.Y);
-          updateCaloriesAndTime(point.X, point.Y);
-          updateInfo(); // 更新資訊
-          currentIndex++; 
-        } else {
-          currentIndex = 0;
-        }
-      })
-      .catch(error => console.error('Error fetching JSON:', error));
-  }
-  
-  // 初始加載icon位置
-  loadjson();
-  
-  // 每秒更新一次位置和相關資訊
-  setInterval(loadjson, 1000);
-  
-  
+let calorieIndex = 0;
+let calorieData = null;
+
+// 初始加載icon位置
+loadjson();
+
+// 每秒更新一次位置
+setInterval(loadjson, 1000);
